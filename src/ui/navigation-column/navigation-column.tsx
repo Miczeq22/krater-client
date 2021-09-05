1176211d73e6eaf2dc8a0fc722bd08e0c1c85135
Mikołaj Wargowski <mikoajw20@gmail.com>
2021-09-05 20:51:36 +0200

    🌈 Menu bootstrap

--- src/ui/navigation-column/navigation-column.tsx
+++ src/ui/navigation-column/navigation-column.tsx
@@ -1,7 +1,18 @@
+import S from './navigation-column.styles';
 import { AvatarContainer } from './parts/avatar-container/avatar-container';
+import { Menu } from './parts/menu/menu';
+import { Tags } from './tags/tags';
 
 export const NavigationColumn = () => (
-  <div className="navigation-column">
-    <AvatarContainer />
-  </div>
+  <S.Container className="navigation-column">
+    <S.Item>
+      <AvatarContainer />
+    </S.Item>
+    <S.Item>
+      <Menu />
+    </S.Item>
+    <S.Item>
+      <Tags />
+    </S.Item>
+  </S.Container>
 );
